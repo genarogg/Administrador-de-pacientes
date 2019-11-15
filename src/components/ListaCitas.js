37108c1e2dc31efdb8027d0df27df24c1b97adcc
Genaro Gonzalez <genarrogg@gmail.com>
2019-11-15 14:56:19 -0400

    Mostrando las Citas del State

--- src/components/ListaCitas.js
+++ src/components/ListaCitas.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Cita from "./Cita"
 
 const ListaCitas = ({citas}) => (
     <div className="card mt-2 py-5">
@@ -6,7 +7,10 @@ const ListaCitas = ({citas}) => (
             <h2 className="card-title text-center">Administra las citas aqui</h2>
             <div className="lista-citas">
                 {citas.map(cita =>(
-                    
+                    <Cita 
+                        key={cita.id}
+                        cita={cita}
+                    />
                 ))}
             </div>
         </div>
