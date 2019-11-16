6409a6f6f336e5a39d02f7ccac38514bf52b4f52
Genaro Gonzalez <genarrogg@gmail.com>
2019-11-15 22:35:24 -0400

    Documentando tus Apps de React con PropTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types"
 
 const Header = ({titulo}) => (
   <header>
@@ -6,4 +7,7 @@ const Header = ({titulo}) => (
   </header>
 );
 
+Header.propTypes = {
+  titulo : PropTypes.string.isRequired
+}
 export default Header;
